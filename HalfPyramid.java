261868f4655c3101552f0e2f47b808784138bdba
Zakaria Shagor <zakaria.tahsinxb@gmail.com>
2025-08-28 16:25:33 +0600

    Nested loop practiced

---
+++ HalfPyramid.java
@@ -0,0 +1,15 @@
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+         // Outer loop for each row
+        for (int i = 1; i <= n; i++) {
+            // Inner loop for printing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
